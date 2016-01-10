80970acc99c2e5cdeac1bbad0f9595167649b1a3
Demin Yin <deminy@deminy.net>
2016-01-09 16:22:13 -0800

    To use Travis CI container-based infrastructure

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
-# Credit: This script was modified based on the script from https://github.com/Behat/WebApiExtension
-# @see https://github.com/Behat/WebApiExtension/blob/master/bin/start_server.sh
 
 set -e
+set -x
 
 touch "$TRAVIS_BUILD_DIR/behat-rest-testing.log"
 
 if [[ "$TRAVIS_PHP_VERSION" == "hhvm" ]]
 then
-    echo "    Installing Nginx"
-    wget http://nginx.org/keys/nginx_signing.key
-    sudo apt-key add nginx_signing.key
-    cat > ".tmp" <<CONF
-deb http://nginx.org/packages/ubuntu/ precise nginx
-CONF
-    cat .tmp | sudo tee -a /etc/apt/sources.list > /dev/null
-    rm .tmp
-    sudo apt-get update -qq
-    sudo apt-get install nginx -qq
+    echo "Installing Nginx."
 
     cat > ".nginx.conf" <<CONF
 events {
@@ -46,12 +36,12 @@ http {
     }
 }
 CONF
-    echo "    Starting the HHVM daemon"
+    echo "Starting the HHVM daemon."
     hhvm --mode server -vServer.Type=fastcgi -vServer.IP='127.0.0.1' -vServer.Port=9000 > "$TRAVIS_BUILD_DIR/behat-rest-testing.log" 2>&1 &
-    echo "    Starting nginx"
+    echo "Starting nginx."
     sudo mkdir -p /var/log/nginx/
     sudo nginx -c "$TRAVIS_BUILD_DIR/.nginx.conf"
 else
-    echo "    Starting the PHP builtin webserver"
+    echo "Starting the PHP builtin web server."
     php -S localhost:8081 www/router.php > /dev/null 2> "$TRAVIS_BUILD_DIR/behat-rest-testing.log" &
 fi
